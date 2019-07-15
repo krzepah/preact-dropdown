15aaa31523eb8f99a361bd55b021b34bc7a240e2
Patrick Borowy <patrick.borowy@gmail.com>
2019-07-15 14:44:24 +0200

    Updated README, linting correction, simple test

--- index.js
+++ index.js
@@ -1,52 +1,64 @@
-import { Component } from 'preact';
+import { Component, h } from 'preact';
 
+/**
+ * A dropdown component
+ * @param children
+ * @param Link => <Link ...args />
+ * @param ...args are sent to Link
+ * @example
+ * <DropDown Link={Button} id={id}>
+ *   <div>
+ *	  My inner content
+ *   </div>
+ * </Dropdown>
+ */
 class Dropdown extends Component {
 	state = { open: false };
-	close = () => (this.setState({open: false}), false)
-    toggle = () => {
-		this.setState({open: !this.state.open});
-	}
-    handleClick = ({target}) => {
+	close = () => (this.setState({ open: false }), false);
+	toggle = () => this.setState({ open: !this.state.open });
+	handleClick = ({ target }) => {
 		if (String(this.props.id) === String(target.id))
-            this.toggle();
-        else if (this.state.open) {
-            do {
-                if (target===this.base) return;
-            } while ((target=target.parentNode))
-            this.close();
-        }
-    }
-    componentDidMount = () => addEventListener(
+			this.toggle();
+		else if (this.state.open) {
+			do {
+				if (target===this.base) return;
+			} while ((target=target.parentNode));
+			this.close();
+		}
+	}
+	componentDidMount = () => addEventListener(
 		'click', this.handleClick
 	)
+	componentDidUpdate = ({ current }) => {
+		if (current!==this.props.current && this.state.open)
+			this.close();
+	}
 	componentWillUnmount = () => removeEventListener(
 		'click', this.handleClick
 	)
-	componentDidUpdate = ({current}) => {
-		if (current!==this.props.current && this.state.open)
-			this.close(); 
-	}
-	render = ({children, Link, ...args}, {open}) => <div
-		ref={button => this.button = button}
-	>
-		<Link
-			onClick={this.toggle}
-			{...args}
-		/>
-		{ open ? children : null }
-	</div>
+	render = ({ children, Link, ...args }, { open }) => (
+		<div ref={button => this.button = button}>
+			<Link {...args} />
+			{ open ? children : null }
+		</div>
+	)
 }
 
+/**
+ * Works just like DropDown but replaces the <Link>
+ */
 class DropReplace extends Dropdown {
-	render = ({children, Link, ...args}, {open}) => <div
-		ref={button => this.button = button}
-	>
-		{ open ? 
+	render = ({ children, Link, ...args }, { open }) => (
+		<div ref={button => this.button = button}>
+			{ open ?
 				children
 				: <Link {...args} />
-		}
-	</div>
+			}
+		</div>
+	)
 }
 
-export default Dropdown;
-export { DropReplace }
+export {
+	Dropdown,
+	DropReplace
+};
---
+++ test/dropdown.test.js
@@ -0,0 +1,19 @@
+import { h } from 'preact';
+import render from 'preact-render-to-string';
+import htmlLooksLike from 'html-looks-like';
+
+import { Dropdown } from '..';
+
+const Link = () => <div>Hello World</div>
+
+describe('Dropdown', () => {
+    it('should render given component', () => {
+        const instance = render(<Dropdown Link={Link} />)
+        const expected = `
+            <div>
+                <div>Hello World</div>
+            </div>
+        `
+        htmlLooksLike(instance, expected);
+    });
+});
